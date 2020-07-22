d0aaa06492c1e1b343360b6cd082e808c1c23512
GilsonJRS <gilsonjuniorcva@gmail.com>
2020-07-22 16:46:02 -0300

    updates

--- main.asm
+++ main.asm
@@ -135,8 +135,12 @@ mean_end_inner_loop:
 	div_else:
 	#print x($s4) and y($f22)
 	#close file
-	li $v0,2
+	
 	mov.s $f12, $f22
+	jal float2string
+	
+	li $v0, 4
+	la $a0, buffer_float
 	syscall
 	
 	li $v0, 4
@@ -391,7 +395,6 @@ string2int_end_loop:
 	lw $ra, 0($sp)
 	add $sp, $sp, 4
 	jr $ra
-	jr 
 #----------------------------------------------
 #string_to_float function
 #$a0 = address of string
@@ -545,16 +548,146 @@ addAlready_exit:
 #$v0 = address os string
 #----------------------------------------------
 int2string:
+	add $sp, $sp, -4
+	sw $ra, ($sp)
+	
 	la $t0, buffer_int
 	slti $t1, $a0, 0 #negative number verification
-	bnez $t1, int2string_negative_verification_continue
+	bne $t1, 1, int2string_negative_verification_continue
 	mul $a0, $a0, -1
 	li $t2, 45 #- signal
 	sb $t2, ($t0)
-	add $t0, $t0, 
+	add $t0, $t0,1 
 int2string_negative_verification_continue:
+	move $a1, $a0
+	move $a0, $t0
+	jal int2string_recursive
 	
+	lw $ra, ($sp)
+	add $sp, $sp, 4
+	jr $ra
+int2string_recursive:
+	addi $sp, $sp, -16
+	sw $ra, 12($sp)
+	sw $s2, 8($sp)
+	sw $s1, 4($sp)
+	sw $s0, 0($sp)
+	
+	li $s2, 0     #i=0
+	move $s0, $a0 #string
+	move $s1, $a1 #num
+	
+	li $t4, 10
+	div $s1, $t4
+	mflo $t4 #get quotient
+	beqz $t4 int2string_recursive_continue #(if n/10 != 0)
+	move $a1, $t4
+	move $a0, $s0
+	jal int2string_recursive #recursive call of int2string
+	move $s2, $v0 #save return
+int2string_recursive_continue:
+	li $t4, 10
+	div $s1, $t4
+	mfhi $t3 #get remainder
+	addi $t3, $t3, '0' #converting to ascii
+	add $s0, $s0, $s2 #moving to correct position on string
+	sb $t3, ($s0)
+	addi $s2, $s2, 1 #update i
+	addi $s0, $s0, 1 #string[i+1]
+	sb $zero,($s0) #end of string(\0)
+	
+	move $v0, $s2 #return
+	move $v1, $s0
 
+	lw $ra, 12($sp)
+	lw $s2, 8($sp)
+	lw $s1, 4($sp)
+	lw $s0, 0($sp)
+	addi $sp, $sp, 16
+	jr $ra
+	
+#----------------------------------------------
+#float2string:
+#arguments:
+#$f12 = float number 
+#return:
+#$v0 = address os string
+#----------------------------------------------
+float2string:
+	#f12 = float
+	#$a0 = string
+	add $sp, $sp, -28
+	swc1 $f22, 24($sp)
+	swc1 $f21, 20($sp)
+	swc1 $f20, 16($sp)
+	sw $s2, 12($sp)
+	sw $s1, 8($sp)
+	sw $s0, 4($sp)
+	sw $ra, 0($sp)
+	
+	la $s0, buffer_float
+	
+	#negative numbers exception
+	mtc1 $zero, $f16
+	cvt.s.w $f16, $f16
+	c.lt.s $f12, $f16
+	bc1f float2string_negative_continue
+	li $t0, -1
+	mtc1 $t0, $f16
+	cvt.s.w $f16, $f16
+	mul.s $f12, $f12, $f16
+	li $t0, '-'
+	sb $t0, ($s0)
+	add $s0, $s0, 1
+float2string_negative_continue:			
+	#converting float to a word, gets int part
+	cvt.w.s $f20, $f12
+	mfc1 $s1, $f20 #int part
+	
+	#geting floor of the float number
+	floor.w.s $f21, $f12
+	cvt.s.w $f21, $f21
+	
+	#removing int part
+	sub.s $f12, $f12, $f21
+
+	#getting two decimal places
+	li $t0, 100
+	mtc1 $t0, $f22
+	cvt.s.w $f22, $f22
+	mul.s $f21, $f12, $f22
+	
+	cvt.w.s $f21, $f21
+	mfc1 $s2, $f21	
+	
+	#converting to char
+	
+	move $a0, $s0
+	move $a1, $s1
+	jal int2string
+	move $s0, $v1 
+										
+	#add point
+	li $t0, 46
+	sb $t0, ($s0)
+	add $s0, $s0, 1
+	
+	move $a0, $s0
+	move $a1, $s2
+	jal int2string
+	move $s0, $v1
+	
+	la $v0, buffer_float
+	
+	lwc1 $f22, 24($sp)
+	lwc1 $f21, 20($sp)
+	lwc1 $f20, 16($sp)
+	lw $s2, 12($sp)
+	lw $s1, 8($sp)
+	lw $s0, 4($sp)
+	lw $ra, 0($sp)
+	add $sp, $sp, 28
+	jr $ra
 #----------------------------------------------
 #error's labels
 #----------------------------------------------
--- testes.asm
+++ testes.asm
@@ -1,27 +1,132 @@
 .data
 	inputFile: .asciiz "/home/gilson/Documents/Computer Organization/mips-project-CO-UTFPR/inputFiles/test.csv" 
-	num: .asciiz "10,12.1"
+	num: .asciiz "12"
 	null: .asciiz ""
 	buffer_input: .space 1024
-	teste: .float 8.77
+	teste: .float 8.10
+	teste2: .float 9.8
 .text
+.main:
+	l.s $f12, teste
+	la $a0, buffer_input
+	jal float2string
+	
+	l.s $f12, teste2
+	la $a0, buffer_input
+	jal float2string
+	
+	#li $v0, 4
+	#la $a0, buffer_input
+	#syscall
+	
+	li $v0, 10
+	syscall
 	#mfhi = remainder mflo = quotient
-int2strig:
-	li $t3, 0
-	move $s0, $a0
-	move $s1, $a1
+float2string:
+	#f12 = float
+	#$a0 = string
+	add $sp, $sp, -16
+	sw $s2, 12($sp)
+	sw $s1, 8($sp)
+	sw $s0, 4($sp)
+	sw $ra, 0($sp)
+	
+	la $s0, buffer_input
+	
+	#negative numbers exception
+	mtc1 $zero, $f16
+	cvt.s.w $f16, $f16
+	c.lt.s $f12, $f16
+	bc1f float2string_negative_continue
+	li $t0, -1
+	mtc1 $t0, $f16
+	cvt.s.w $f16, $f16
+	mul.s $f12, $f12, $f16
+	li $t0, '-'
+	sb $t0, ($s0)
+	add $s0, $s0, 1
+float2string_negative_continue:			
+	#converting float to a word, gets int part
+	cvt.w.s $f20, $f12
+	mfc1 $s1, $f20 #int part
+	
+	
+	#geting floor of the float number
+	floor.w.s $f21, $f12
+	cvt.s.w $f21, $f21
+	
+	#removing int part
+	sub.s $f12, $f12, $f21
+
+	#getting two decimal places
+	li $t0, 100
+	mtc1 $t0, $f22
+	cvt.s.w $f22, $f22
+	mul.s $f21, $f12, $f22
+	
+	cvt.w.s $f21, $f21
+	mfc1 $s2, $f21	
+	
+	#converting to char
+	
+	move $a0, $s0
+	move $a1, $s1
+	jal int2string
+	move $s0, $v1 
+
+	#add point
+	li $t0, 46
+	sb $t0, ($s0)
+	add $s0, $s0, 1
+	
+	move $a0, $s0
+	move $a1, $s2
+	jal int2string
+	
+	li $v0, 4
+	la $a0,buffer_input
+	syscall 
+	
+	lw $ra, 0($sp)
+	add $sp, $sp, 4
+	jr $ra
+int2string:
+	addi $sp, $sp, -16
+	sw $ra, 12($sp)
+	sw $s2, 8($sp)
+	sw $s1, 4($sp)
+	sw $s0, 0($sp)
+	
+	li $s2, 0     #i=0
+	move $s0, $a0 #string
+	move $s1, $a1 #num
 	
 	li $t4, 10
 	div $s1, $t4
-	mfhi $t4
-	beqz $t4 int2string_continue
+	mflo $t4 #get quotient
+	beqz $t4 int2string_continue #(if n/10 != 0)
 	move $a1, $t4
 	move $a0, $s0
-	jal int2string
-	move $t3, $v0
+	jal int2string #recursive call of int2string
+	move $s2, $v0 #save return
 int2string_continue:
 	li $t4, 10
 	div $s1, $t4
-	add $t3
-
+	mfhi $t3 #get remainder
+	addi $t3, $t3, '0' #converting to ascii
+	add $s0, $s0, $s2 #moving to correct position on string
+	sb $t3, ($s0)
+	addi $s2, $s2, 1 #update i
+	addi $s0, $s0, 1 #string[i+1]
+	sb $zero,($s0) #end of string(\0)
 	
+	
+	move $v0, $s2 #return
+	move $v1, $s0
+
+	lw $ra, 12($sp)
+	lw $s2, 8($sp)
+	lw $s1, 4($sp)
+	lw $s0, 0($sp)
+	addi $sp, $sp, 16
+	jr $ra	
